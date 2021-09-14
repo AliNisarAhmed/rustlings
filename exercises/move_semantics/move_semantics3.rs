463a6c858e42797cca55c6d60840649edeedc03a
Ali Nisar Ahmed <ali.nisar87@gmail.com>
2021-09-13 23:11:49 -0600

    collections

--- exercises/move_semantics/move_semantics3.rs
+++ exercises/move_semantics/move_semantics3.rs
@@ -5,9 +5,9 @@
 
 
 fn main() {
-    let mut vec0 = Vec::new();
+    let vec0 = Vec::new();
 
-    let mut vec1 = fill_vec(&mut vec0);
+    let mut vec1 = fill_vec(vec0);
 
     println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
 
@@ -16,7 +16,7 @@ fn main() {
     println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
 }
 
-fn fill_vec(vec: &mut Vec<i32>) -> &mut Vec<i32> {
+fn fill_vec(mut vec: Vec<i32>) -> Vec<i32> {
     vec.push(22);
     vec.push(44);
     vec.push(66);
